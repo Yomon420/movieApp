9b1141b116a78ba94704fc2f17e6ac7c84eb73f9
yamin <yaminhany12@gmail.com>
2023-10-01 23:29:23 +0300

    -added option to select language in filter (most work on that is in moveSearch.dart -added flags in assets -removed option to call api by country -made 1+ things nullable in movieFilters.dart -added &with_original_language=$language in movieApi.dart -select an option = it gets a green border

--- lib/Main_Flow/movieSearch.dart
+++ lib/Main_Flow/movieSearch.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/Main_Flow/movieSearchResults.dart';
-
 import '../Helper_Widgets/movieFilters.dart';
 import '../classes/movieObject.dart';
 import '../components/Text.dart';
 import '../Helper_Widgets/movieAPI.dart';
+
 class MovieSearch extends StatefulWidget {
-  MovieSearch({super.key});
+  const MovieSearch({super.key});
   @override
   State<MovieSearch> createState() => _MovieSearchState();
 }
 
 class _MovieSearchState extends State<MovieSearch> {
-
   // -------------------------------------------------------------
   List<String> selectedGenres = [];
+  String? selectedLanguage;
+  bool? isSelected = false;
   // Function to handle genre selection
+
+  void toggleLanguageSelection(String language){
+    setState(() {
+      selectedLanguage = language;
+      if(!isSelected!){
+        isSelected = true;
+        print("Selected Language: $selectedLanguage");
+      }
+      else if(isSelected!){
+        isSelected = false;
+        print("Unselected Language: $selectedLanguage");
+      }
+    });
+  }
+
   void toggleGenreSelection(String genre) {
     setState(() {
       if (selectedGenres.contains(genre)) {
@@ -26,13 +42,14 @@ class _MovieSearchState extends State<MovieSearch> {
       }
     });
   }
+
   // LIST OF GENRES FOR MANY PURPOSES, GET PICS FROM ASSETS, OUTPUT NAMES
   List<String> genres = [
     'Action',
     'Drama',
+    'Fantasy',
     'Comedy',
     'Animation',
-    'Fantasy',
     'Horror',
     'War',
     'Romance'
@@ -42,62 +59,130 @@ class _MovieSearchState extends State<MovieSearch> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        physics: const ScrollPhysics(),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 200,
-              child: ListView.builder(
-                itemCount: genres.length,
-                itemBuilder: (context, index) {
-                  return MovieFilter(
-                    imagePath: "assets/${genres[index]}.jpeg",
-                    genreName: genres[index],
-                    onTap: () {
-                      // Call the toggleGenreSelection function when the MovieFilter is tapped
-                      toggleGenreSelection(genres[index]);
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+            physics: const ScrollPhysics(),
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: ListView.builder(
+                    itemCount: genres.length,
+                    itemBuilder: (context, index) {
+                      return MovieFilter(
+                        imagePath: selectedGenres.contains(genres[index]) ? "assets/${genres[index]}_selected.png" : "assets/${genres[index]}.png",
+                        genreName: genres[index],
+                        onTap: () {
+                          // Call the toggleGenreSelection function when the MovieFilter is tapped
+                          toggleGenreSelection(genres[index]);
+                        },
+                        isSelected: selectedGenres.contains(genres[index]), // Pass isSelected flag for ui changes (YOUNIS)
+                      );
                     },
-                    isSelected: selectedGenres.contains(genres[index]), // Pass isSelected flag for ui changes (YOUNIS)
-                  );
-                },
-                scrollDirection: Axis.horizontal,
-              ),
-            ),
-            SizedBox(height: 100,),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue, shape: const StadiumBorder()),
-                onPressed: () async {
-                  // APPLY ALL THE FILTERS HERE
-                  var api = Api(
-                    // year: 2024, // THIS WILL SHOW ALL MOVIES BEFORE THIS YEAR
-                    withGenres: selectedGenres, // HERE IS MY GENRE FILTER!
-                    voteAverage: 7, // THIS WILL SHOW ALL MOVIES ABOVE THIS RATING
-                    // country: "US", // ONLY USE ALPHA 2 COUNTRY CODES
-                    runtimeChoice: "short", // RUNTIME FILTERS: "normal", "short", "long"
-                    page: 1, // Replace with the desired page number
-                  );
-                  // Here is where all the magic happens
-                  var url = api.CallMovieFilterApi();
-                  var json = await api.get(url: url);
-                  Movie movie = Movie.fromJson(json);
-                  List<MovieResult> movieResults = movie.results;
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                SizedBox(height: 100,),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      MovieFilter(
+                        imagePath: "assets/USA.png",
+                        onTap: (){
+                          toggleLanguageSelection("en");
+                        },
+                        isSelected: selectedLanguage == "en",
+                      ),
+                      MovieFilter(
+                        imagePath: "assets/ARABIC.jpg",
+                        onTap: (){
+                          toggleLanguageSelection("ar");
+                        },
+                        isSelected: selectedLanguage == "ar",
+                      ),
+                      MovieFilter(
+                        imagePath: "assets/KO.png",
+                        onTap: (){
+                          toggleLanguageSelection("ko");
+                        },
+                        isSelected: selectedLanguage == "ko",
+                      ),
+                      MovieFilter(
+                        imagePath: "assets/INDIA.png",
+                        onTap: (){
+                          toggleLanguageSelection("hi");
+                        },
+                        isSelected: selectedLanguage == "hi",
+                      ),
+                      MovieFilter(
+                        imagePath: "assets/JAPAN.png",
+                        onTap: (){
+                          toggleLanguageSelection("ja");
+                        },
+                        isSelected: selectedLanguage == "ja",
+                      ),
+                      MovieFilter(
+                        imagePath: "assets/FRANCE.png",
+                        onTap: (){
+                          toggleLanguageSelection("fr");
+                        },
+                        isSelected: selectedLanguage == "fr",
+                      ),
+                      MovieFilter(
+                        imagePath: "assets/SPAIN.png",
+                        onTap: (){
+                          toggleLanguageSelection("es");
+                        },
+                        isSelected: selectedLanguage == "es",
 
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                    return MovieSearchResults(movieResults: movieResults);
-                  },));
-                },
-                child: const CustomText(
-                  "Search",
-                  size: 24,
-                  color: Colors.white,
-                )),
-          ],
-        ),
-      ),
-    );
-  }
+                      ),
+                      MovieFilter(
+                        imagePath: "assets/OTHER.jpg",
+                        onTap: (){
+                          toggleLanguageSelection("other");
+                        },
+                        isSelected: selectedLanguage == "other",
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue, shape: const StadiumBorder()),
+                    onPressed: () async {
+                      // APPLY ALL THE FILTERS HERE
+                      var api = Api(
+                        year: 2018,
+                        withGenres: selectedGenres,
+                        voteAverage: 2,
+                        runtimeChoice: "normal",
+                        language: selectedLanguage,
+                        page: 1,
+                      );
+                      // Here is where all the magic happens
+                      var url = api.CallMovieFilterApi();
+                      var json = await api.get(url: url);
+                      Movie movie = Movie.fromJson(json);
+                      List<MovieResult> movieResults = movie.results;
+
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) {
+                          return MovieSearchResults(movieResults: movieResults);
+                        },
+                      ));
+                    },
+                    child: const CustomText(
+                      "Search",
+                      size: 24,
+                      color: Colors.white,
+                    )),
+              ],
+            ),
+            ),
+        );
+    }
 }
